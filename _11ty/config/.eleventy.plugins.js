bfbd5d22c5ea00ed34f23b951ecbeeb35f5fae0f
Sindre Bøyum <boyum@users.noreply.github.com>
2021-05-25 15:45:58 +0200

    Add TypeScript types (#121)

--- _11ty/config/.eleventy.plugins.js
+++ _11ty/config/.eleventy.plugins.js
@@ -1,7 +1,13 @@
+// @ts-check
+/// <reference path="../../index.d.ts" />
+
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginPWA = require('eleventy-plugin-pwa');
 
+/**
+ * @param {EleventyConfig} eleventyConfig
+ */
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
--- _11ty/config/.eleventy.shortcodes.js
+++ _11ty/config/.eleventy.shortcodes.js
@@ -1,16 +1,24 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addShortcode("codepen", function(url) {
-    const penUrl = url.includes('embed') ? url : url.replace(/(\/full\/|\/details\/|\/pen\/)/ig, '/embed/');
+// @ts-check
+/// <reference path="../../index.d.ts" />
+
+/**
+ *
+ * @param {EleventyConfig} eleventyConfig
+ */
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addShortcode("codepen", (/** @type {string} */ url) => {
+    const penUrl = url.includes("embed")
+      ? url
+      : url.replace(/(\/full\/|\/details\/|\/pen\/)/gi, "/embed/");
     const html = String.raw;
 
-    return html `
-<iframe
-  allowtransparency="true"
-  frameborder="no"
-  height="600"
-  src="${penUrl}?height=600&amp;default-tab=result&amp;embed-version=2"
-  scrolling="no"
-  style="width: 100%;"
-></iframe>`;
+    return html`<iframe
+      allowtransparency="true"
+      frameborder="no"
+      height="600"
+      src="${penUrl}?height=600&amp;default-tab=result&amp;embed-version=2"
+      scrolling="no"
+      style="width: 100%;"
+    ></iframe>`;
   });
 };
