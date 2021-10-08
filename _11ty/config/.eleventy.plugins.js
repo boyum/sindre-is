8281c8caff71ecf7ab2ce876c5f1604bc85c69a6
Sindre Bøyum <boyum@users.noreply.github.com>
2021-10-08 08:08:41 +0200

    install @11ty v1 (#131)

--- _11ty/config/.eleventy.plugins.js
+++ _11ty/config/.eleventy.plugins.js
@@ -3,26 +3,29 @@
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginPWA = require('eleventy-plugin-pwa');
+const pluginPWA = require("eleventy-plugin-pwa");
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 /**
  * @param {EleventyConfig} eleventyConfig
  */
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.addPlugin(UpgradeHelper);
 
   eleventyConfig.addPlugin(pluginPWA, {
     clientsClaim: true,
     skipWaiting: true,
-    runtimeCaching: [{
+    runtimeCaching: [
+      {
         urlPattern: /\/img\/.+\.(?:png|gif|jpg|jpeg|webp|svg)$/,
         handler: "cacheFirst",
       },
       {
         urlPattern: "/service-worker.js",
         handler: "networkFirst",
-      }
+      },
     ],
   });
 };
